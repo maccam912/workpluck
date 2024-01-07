ab59e3b4b768256f7317391ac2ba16bd55afc80f
Matt Koski <maccam912@gmail.com>
2024-01-06 22:46:51 -0600

    Add task status and timestamp to handleTaskSubmit function

--- main.go
+++ main.go
@@ -77,6 +77,9 @@ func handleTaskSubmit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	task.ID = uuid.New().String()
+	task.Status = "new"
+	task.Timestamp = time.Now()
+
 	storeMutex.Lock()
 	taskStore[task.ID] = task
 	storeMutex.Unlock()
